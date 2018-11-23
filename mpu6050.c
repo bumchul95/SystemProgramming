0a493102b202f57cb18ffc190f8de3bc5fc8b17f
hwangbumchul <bumchul95@naver.com>
2018-11-23 17:56:03 +0000

    insert gyro with JUSUCK

--- mpu6050.c
+++ mpu6050.c
@@ -11,6 +11,7 @@
 
 #define Device_Address 0x68 //check MPU6050's address from i2cdetect -y -r 1
 
+// these are MPU6050 Registers and their Address
 #define PWR_MGMT_1 0x6B
 #define SMPLRT_DIV 0x19
 #define CONFIG 0x1A
@@ -25,18 +26,25 @@
 
 int fd;
 
-void MPU6050_Init(){
+void MPU6050_Init(){// they use address because of I2C
+	//write to sample rate register
 	wiringPiI2CWriteReg8(fd, SMPLRT_DIV, 0x07);
+	//write to power management register
 	wiringPiI2CWriteReg8(fd ,PWR_MGMT_1, 0x01);
+	//write to configuration register
 	wiringPiI2CWriteReg8(fd, CONFIG, 0);
+	//write to Gyro configuration register
 	wiringPiI2CWriteReg8(fd, GYRO_CONFIG, 24);
+	//write to Interrupt enable register
 	wiringPiI2CWriteReg8(fd, INT_ENABLE, 0x01);
 }
 
 short read_raw_data(int addr){
+	//Accel and gyro value are 16-bit
 	short high_byte, low_byte, value;
 	high_byte = wiringPiI2CReadReg8(fd, addr);
 	low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	//concatenate high low values
 	value = (high_byte << 8) | low_byte;
 	return value;
 }
@@ -53,26 +61,32 @@ int main(void){
 	float Ax=0, Ay=0, Az=0;
 	float Gx=0, Gy=0, Gz=0;
 	fd = wiringPiI2CSetup(Device_Address);
-	MPU6050_Init();
+	MPU6050_Init(); //setting
 
 	while(1){
+		//read raw value of Acc and Gyro from MPU
 		Acc_x = read_raw_data(ACCEL_XOUT_H);
 		Acc_y = read_raw_data(ACCEL_YOUT_H);
 		Acc_z = read_raw_data(ACCEL_ZOUT_H);
-
+		
 		Gyro_x = read_raw_data(GYRO_XOUT_H);
 		Gyro_y = read_raw_data(GYRO_YOUT_H);
 		Gyro_z = read_raw_data(GYRO_ZOUT_H);
-
+		
+		//printf("Gx=%d Gy=%d Gz=%d Ax=%d Ay=%d Az=%d\n", Acc_x, Acc_y, Acc_z, Gyro_x, Gyro_y, Gyro_z);
+		
+		
+		//divide raw value by sensitivity scale factor
+		// Accelerometer K-axis data in g
 		Ax = Acc_x / 16384.0;
 		Ay = Acc_y / 16384.0;
 		Az = Acc_z / 16384.0;
-
+		//Gyro K-axis data in degree/seconds
 		Gx = Gyro_x/131;
 		Gy = Gyro_y/131;
 		Gz = Gyro_z/131;
 
-		printf("\n Gx=%.3f Gy=%.3f Gz=%.3f Ax=%.3fg Ay=%.3fg Az=%.3fg\n", Gx, Gy, Gz, Ax, Ay, Az);
+		printf("Gx=%.3f Gy=%.3f Gz=%.3f Ax=%.3fg Ay=%.3fg Az=%.3fg\n", Gx, Gy, Gz, Ax, Ay, Az);
 		delay(500);
 	}
 
