56179f133700ac9680bbac8dabe2cab54f411575
YeongUn <gotnanagi@gamil.com>
2018-11-23 18:30:11 +0000

    Fixed untracked files and add WiringPi

---
+++ WiringPi/touch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define PIR 4
+
+int touch(void){
+	if(wiringPiSetup() == -1)
+		return -1;
+
+	pinMode(PIR,INPUT);
+	
+	if(digitalRead(PIR) == 0){
+		printf("not detection\n");
+		delay(1000);
+		return digitalRead(PIR);
+	}else{
+		printf("detection\n");
+		delay(1000);
+		return digitalRead(PIR);
+	}
+}
+
