b97615601f7580b44022abd3b1e03a774a978c1b
YeongUn <gotnanagi@gamil.com>
2018-12-06 13:53:25 +0000

    Add socket commute  and dcmotor

---
+++ dcmotor/client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+int main(void){
+	int sockfd;
+	char * button=malloc(sizeof(char)*10);
+	struct sockaddr_in servaddr;	
+	if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0){
+		perror("socket create fail");
+		exit(1);
+	}
+
+	memset(&servaddr,0,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_port=htons(4444);
+	servaddr.sin_addr.s_addr=inet_addr("192.168.18.128");
+
+	sleep(1);
+	printf("test..1 \n");
+	int ret;
+	while(1){
+	  scanf("%s",button);
+	  ret= sendto(sockfd,button,strlen(button)+1,0,(const struct sockaddr *)&servaddr,sizeof(servaddr));
+	  printf("send msg with %d bytes \n",ret);
+	
+	}
+	return 0;
+}
---
+++ dcmotor/server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+#include <wiringPi.h>
+
+#define MOTOR1A 1
+#define MOTOR1B 2
+
+#define BUFF_SIZE 1024
+
+int main(void){
+	int sock;
+	int client_addr_size;
+	int rcv_data;
+	struct sockaddr_in server_addr;	
+	struct sockaddr_in client_addr;
+
+	char buff_rcv[BUFF_SIZE+5];
+	char buff_snd[BUFF_SIZE+5];
+	
+ 	if(wiringPiSetup()==-1){
+		return -1;
+	}
+
+	pinMode(MOTOR1A,OUTPUT);
+	pinMode(MOTOR1B,OUTPUT);
+
+	if((sock=socket(AF_INET,SOCK_DGRAM,0))<0){
+		perror("socket create fail");
+		exit(1);
+	}
+
+	memset(&server_addr,0,sizeof(server_addr));
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_port=htons(4444);
+	server_addr.sin_addr.s_addr=inet_addr("192.168.18.128");
+	
+	if( -1 == bind(sock,(struct sockaddr*)&server_addr,sizeof(server_addr)))
+	{
+		perror("bind is error");
+		exit(1);
+	}
+
+	sleep(2);
+	int j=0;
+	while(1){
+	  printf("test..i\n");
+	  rcv_data=recvfrom(sock,buff_rcv,BUFF_SIZE,0,(struct sockaddr*)&client_addr,sizeof(client_addr));
+	  printf("receive is %s ,%d \n",buff_rcv,rcv_data);
+	  if(strcmp(buff_rcv,"on")==0){
+
+	  	for(j=0;j<10;j++){
+		  digitalWrite(MOTOR1A,1);
+		  digitalWrite(MOTOR1B,0);
+
+		  delay(1000);
+
+		  digitalWrite(MOTOR1A,0);
+		  digitalWrite(MOTOR1B,0);
+ 		  
+		  delay(1000);	
+		}
+	  }
+	  //ret= sendto(sockfd,(const char*)hello,strlen(hello),0,(const struct sockaddr *)&servaddr,sizeof(servaddr));
+	 
+	  //printf("send msg with %d bytes \n",ret);
+	}
+
+	return 0;
+
+}
