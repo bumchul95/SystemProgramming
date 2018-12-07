188ee7edd1cfc1e47b63d9850d5c516f3ca9425b
hwangbumchul <bumchul95@naver.com>
2018-12-07 15:29:45 +0000

    led

--- led/.tmp_versions/led_dev.mod
+++
@@ -1,3 +0,0 @@
-/home/pi/Documents/systemprogramming/led/led_dev.ko
-/home/pi/Documents/systemprogramming/led/led_dev.o
-
--- led_dev.c
+++ led_dev.c
@@ -8,7 +8,7 @@
 #include <linux/slab.h>
 #include <linux/cdev.h>
 #define LED	18
-//#define LED_B	4
+#define LED_B	4
 #define DEV_NAME "led_dev"
 #define DEV_NUM 240
 
@@ -18,8 +18,8 @@ int led_open(struct inode *pinode, struct file *pfile) {
 	printk(KERN_ALERT "OPEN led_dev\n");
 	gpio_request(LED, "LED");
 	gpio_direction_output(LED, 0);
-	//gpio_request(LED_B, "LED_B");
-	//gpio_direction_output(LED_B, 0);
+	gpio_request(LED_B, "LED_B");
+	gpio_direction_output(LED_B, 0);
 
 	return 0;
 }
@@ -27,7 +27,7 @@ int led_open(struct inode *pinode, struct file *pfile) {
 int led_close(struct inode *pinode, struct file *pfile) {
 	printk(KERN_ALERT, "RELEASE led_dev\n");
 	gpio_set_value(LED, 0);
-	//gpio_set_value(LED_B, 0);
+	gpio_set_value(LED_B, 0);
 	mdelay(1);
 	return 0;
 }
@@ -36,21 +36,19 @@ ssize_t led_write(struct file *pfile, const char *buffer, size_t length, loff_t 
 	printk("Write led sensor\n");
 	int i;
 	
-	/*msg = (char *)kmalloc(32, GFP_KERNEL);
 	if(copy_from_user(msg,buffer,length)){
 		printk("Write Error\n");
 		return -1;
 	}
 	printk("length : %d\n", length);
-	*/
-	//if(length == 1){
+	
+	if(length == 1){
 		for(i=0;i<50;i++){
 		gpio_set_value(LED, 1);
 		mdelay(19.9);
 		gpio_set_value(LED, 0);
 		mdelay(0.1);
 		}
-
 		for(i=0;i<50;i++){
 		gpio_set_value(LED, 1);
 		mdelay(16);
@@ -75,17 +73,39 @@ ssize_t led_write(struct file *pfile, const char *buffer, size_t length, loff_t 
 		gpio_set_value(LED, 0);
 		mdelay(16);
 		}
-	//}
-	/*
+	}
+	
 	else if(length == 2){
 		for(i=0;i<50;i++){
 		gpio_set_value(LED_B, 1);
-		mdelay(19.9);
+		mdelay(4);
 		gpio_set_value(LED_B, 0);
-		mdelay(0.1);
+		mdelay(16);
+		}
+		for(i=0;i<50;i++){
+		gpio_set_value(LED_B, 1);
+		mdelay(8);
+		gpio_set_value(LED_B, 0);
+		mdelay(12);
+		}
+		for(i=0;i<50;i++){
+		gpio_set_value(LED_B, 1);
+		mdelay(12);
+		gpio_set_value(LED_B, 0);
+		mdelay(8);
+		}
+		for(i=0;i<50;i++){
+		gpio_set_value(LED_B, 1);
+		mdelay(16);
+		gpio_set_value(LED_B, 0);
+		mdelay(4);
+		}
+		for(i=0;i<50;i++){
+		gpio_set_value(LED_B, 1);
+		mdelay(20);
 		}
 	}
-	*/
+	
 	return length;
 }
 
